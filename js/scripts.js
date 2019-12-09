d3ee847406223b1979e15ed9209c168856e34b8e
d00d1e <lee.julianay@gmail.com>
2019-12-08 19:49:40 -0800

    Added modal
    added modal function to home page buttons

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+(function(){
+	var modalContainer = [...document.querySelectorAll('.modal-container')];
+
+
+    //show modal 
+    var modalButton = [...document.querySelectorAll('.modal-button')];
+	modalButton.forEach(function(btn){
+		btn.onclick = function() {
+			var modal = btn.getAttribute('data-modal');
+			document.getElementById(modal).style.display = "block";
+		}
+    });
+    
+    //hide modal
+    var closeButton = [...document.querySelectorAll(".modal-close")]
+    closeButton.forEach(function(btn) {
+        btn.onclick = function() {
+            var modal = btn.closest('.modal-container');
+            modal.style.display = 'none';
+        }
+    });
+    
+    //close modal when user clicks outside the modal
+    window.onclick = function(e) {
+        if (e.target.className === "modal-container") {
+            e.target.style.display = 'none';
+        }
+    };
+
+	
+})();
+
+
+
