1fde41fd4875c425cc55004b9545d330882b0050
d00d1e <lee.julianay@gmail.com>
2020-06-11 12:25:10 -0700

    updated projects list

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 (function(){
 	var modalContainer = [...document.querySelectorAll('.modal-container')];
 
-
-    //show modal 
-    var modalButton = [...document.querySelectorAll('.modal-button')];
+  //show modal 
+  var modalButton = [...document.querySelectorAll('.modal-button')];
 	modalButton.forEach(function(btn){
 		btn.onclick = function() {
 			var modal = btn.getAttribute('data-modal');
@@ -11,23 +10,22 @@
 		}
     });
     
-    //hide modal
-    var closeButton = [...document.querySelectorAll(".modal-close")]
-    closeButton.forEach(function(btn) {
-        btn.onclick = function() {
-            var modal = btn.closest('.modal-container');
-            modal.style.display = 'none';
-        }
-    });
-    
-    //close modal when user clicks outside the modal
-    window.onclick = function(e) {
-        if (e.target.className === "modal-container") {
-            e.target.style.display = 'none';
-        }
-    };
+  //hide modal
+  var closeButton = [...document.querySelectorAll(".modal-close")]
+  closeButton.forEach(function(btn) {
+      btn.onclick = function() {
+        var modal = btn.closest('.modal-container');
+        modal.style.display = 'none';
+      }
+  });
+  
+  //close modal when user clicks outside the modal
+  window.onclick = function(e) {
+    if (e.target.className === "modal-container") {
+        e.target.style.display = 'none';
+    }
+  };
 
-	
 })();
 
 
